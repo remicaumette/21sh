08fb71494f5556dc56ae50436baa95b05ad2fb64
Timothee FUZEAU <tifuzeau@student.42.fr>
2019-01-16 13:21:28 +0100

    [fix] ctrl + l. clear

--- src/shell/action/action_clear.c
+++ src/shell/action/action_clear.c
@@ -6,7 +6,7 @@
 /*   By: timfuzea <tifuzeau@student.42.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/06/28 16:48:33 by timfuzea     #+#   ##    ##    #+#       */
-/*   Updated: 2019/01/15 14:39:45 by timfuzea    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/01/16 13:15:34 by timfuzea    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,6 +15,7 @@
 
 int		action_clear(t_shell *shell)
 {
+	int		i;
 	char	*tmp;
 
 	if ((tmp = tgetstr(TC_CLEAR, NULL)) == NULL)
@@ -22,6 +23,20 @@ int		action_clear(t_shell *shell)
 	ft_putstr(tmp);
 	shell_prompt(shell);
 	ft_putstr(shell->line->content);
+	i = shell->line->size + 1;
+	if (shell->line->cursor != i)
+	{
+		if ((tmp = tgetstr(TC_INSER_START, NULL)) == NULL)
+			return (FAIL);
+		ft_putstr(tmp);
+		while (i != shell->line->cursor)
+		{
+			if ((tmp = tgetstr(TC_MOVE_LEFT, NULL)) == NULL)
+				return (FAIL);
+			ft_putstr(tmp);
+			i--;
+		}
+	}
 	return (SUCCESS);
 }
 

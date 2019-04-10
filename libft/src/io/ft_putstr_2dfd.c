8afb43c298600ce8263997263635e355039b06de
Timothee FUZEAU <tifuzeau@student.42.fr>
2019-04-10 22:29:09 +0200

    [fix] seg

--- libft/src/io/ft_putstr_2dfd.c
+++ libft/src/io/ft_putstr_2dfd.c
@@ -6,7 +6,7 @@
 /*   By: timfuzea <tifuzeau@student.42.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/12/06 21:18:52 by timfuzea     #+#   ##    ##    #+#       */
-/*   Updated: 2017/12/06 21:18:52 by timfuzea    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/10 22:27:54 by timfuzea    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,10 +15,13 @@
 
 void	ft_putstr_2dfd(char **str, int fd)
 {
-	while (*str)
+	if (str)
 	{
-		write(fd, *str, ft_strlen(*str));
-		write(fd, "\n", 1);
-		str++;
+		while (*str)
+		{
+			write(fd, *str, ft_strlen(*str));
+			write(fd, "\n", 1);
+			str++;
+		}
 	}
 }
